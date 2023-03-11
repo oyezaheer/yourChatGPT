29129dafb60a8601fbaf8b5e6cf51c226b503155
oyezaheer <zaheer286khan@gmail.com>
2023-03-11 19:16:30 +0530

    update the server url

--- client/script.js
+++ client/script.js
@@ -75,7 +75,7 @@ const handleSubmit = async (e) => {
 
   // fetch data from server -> bots reponse
 
-  const response = await fetch('http://localhost:5000', {
+  const response = await fetch('https://yourchatgpt.onrender.com', {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json'
